d4cc548a0f21f89c8fe69ca42bb84c06e43b85dd
kivancguncavdi <kivancguncavdi@gmail.com>
2018-06-28 08:21:21 -0700

    sortFunc bugfix and prj deleted.

--- sortFunc.c
+++ sortFunc.c
@@ -1,12 +1,14 @@
 int sortFunc(int* arr, int n) {
     int temp = 0;
-    for(int i=0; i<n-1; i++) {
-        if(arr[i]>arr[i+1]) {
-            temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp;
-        }
-    }
+    for(int k=0; k<n; k++) {
+	for(int i=0; i<n-1; i++) {
+		if(arr[i]>arr[i+1]) {
+		    temp = arr[i];
+		    arr[i] = arr[i+1];
+		    arr[i+1] = temp;
+		}
+	    }
+	}
 
     return 0;
 }
