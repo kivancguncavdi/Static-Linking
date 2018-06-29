530e856148234e03edf8380517590ed88e080c3e
kivancguncavdi <kivancguncavdi@gmail.com>
2018-06-29 00:25:45 -0700

    bubble sort algorithm improved

--- sortFunc.c
+++ sortFunc.c
@@ -1,14 +1,20 @@
 int sortFunc(int* arr, int n) {
     int temp = 0;
-    for(int k=0; k<n; k++) {
+    int flag = 0;
+    
+    while(1) {
+	flag = 1;
 	for(int i=0; i<n-1; i++) {
 		if(arr[i]>arr[i+1]) {
 		    temp = arr[i];
 		    arr[i] = arr[i+1];
 		    arr[i+1] = temp;
+		    flag = 0;
 		}
 	    }
+	if(flag != 0)
+		break;
 	}
-
+	
     return 0;
 }
